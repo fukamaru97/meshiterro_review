4b21fe1b2802372c20f50811ffecc66bb38a13fa
EC2 Default User <taichibonbarx@gmail.com>
2022-08-13 10:03:24 +0000

    アプリ完成2_19章

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:show, :edit, :update]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create, :destroy]
-  end
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
+
+  resources :users, only: [:show, :edit, :update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
